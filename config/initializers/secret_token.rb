4949094c5002cee963e75002dedd6e185864da3f
방정현 <zahoku@gmail.com>
2013-09-02 20:17:52 +0900

    프로젝트 생성

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MissionComplete::Application.config.secret_key_base = '95842f5c62f214d3eeeb3bad05bfcec13b91ede5f4d07e543acef8ff71a9a41831bfb8037c1bdfedf46871aa4702f8001ce615f9722a2faf1d5406a69f5882f9'
