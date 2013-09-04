37c1152539b96c1f637d56bc0a2640eaaa8d892b
방정현 <zahoku@gmail.com>
2013-09-04 22:29:28 +0900

    열라 짱

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 MissionComplete::Application.routes.draw do
+  resources :posts do
+    resources :comments
+  end
+
   get "home/index"
+  root :to => "home#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
